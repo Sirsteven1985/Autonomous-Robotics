0bf985ec6dfbab2704938e0330e0e7cd7fbf3220
PAUL <PAUL@localhost>
2018-02-05 21:54:26 -0800

    Add files for direct register write

---
+++ Lab2/Lab2 Direct Register Writes/M1PWMn.c
@@ -0,0 +1,47 @@
+/*
+ * M1PWMn.c
+ *
+ *  Created on: Jan 30, 2018
+ *      Author: vuong
+ */
+
+#include "MnPWMn.h"
+#include <stdint.h>
+#include "SYSCTL.h"
+
+void initM1PWMn(void) {
+
+    //Enable clock to M1PWMn
+    SYSCTL[SYSCTL_RCGCPWM] |= SYSCTL_RCGC_M1PWMn;
+    SYSCTL[SYSCTL_RCGCPWM] |= SYSCTL_RCGC_M1PWMn;
+
+    for(uint32_t i = 0; i < 0xFFFF; i++);      // Need extra delay for PWM clock to set.
+
+//    6. Configure the PWM generator for countdown mode with immediate updates to the parameters.
+//    ■ Write the PWM0CTL register with a value of 0x0000.0000.
+        M1PWMn[PWM0_CTL] &= 0x00000000;
+//    ■ Write the PWM0GENA register with a value of 0x0000.008C.
+        //M1PWMn[PWM1_GENA] = 0x000000B0;
+//    ■ Write the PWM0GENB register with a value of 0x0000.080C.
+        //M1PWMn[PWM1_GENB] &= ~0x80c;
+        M1PWMn[PWM0_GENB] |= (0x80c << 0);
+//    7. Set the period. For a 25-KHz frequency, the period = 1/25,000, or 40 microseconds. The PWM
+//    clock source is 10 MHz; the system clock divided by 2. Thus there are 400 clock ticks per period.
+//    Use this value to set the PWM0LOAD register. In Count-Down mode, set the LOAD field in the
+//    PWM0LOAD register to the requested period minus one.
+//    ■ Write the PWM0LOAD register with a value of 0x0000.018F.
+        M1PWMn[PWM0_LOAD] |= (0x18F << 0);                                 //399 decimal
+//    8. Set the pulse width of the MnPWM0 pin for a 25% duty cycle.
+//    ■ Write the PWM0CMPA register with a value of 0x0000.012B.
+        //M1PWMn[PWM1_CMPA] = 0x0000012B;                                 //299 decimal
+//    9. Set the pulse width of the MnPWM1 pin for a 75% duty cycle.
+//    ■ Write the PWM0CMPB register with a value of 0x0000.0063.
+        M1PWMn[PWM0_CMPB] |= (0x12B << 0);
+//    10. Start the timers in PWM generator 0.
+//    ■ Write the PWM0CTL register with a value of 0x0000.0001.
+        M1PWMn[PWM0_CTL] |= (1 << 0);
+//    11. Enable PWM outputs.
+//    ■ Write the PWMENABLE register with a value of 0x0000.0002.
+        M1PWMn[PWM_ENABLE] |= (1 << 1);
+
+}
---
+++ Lab2/Lab2 Direct Register Writes/SYSCTL.h
@@ -0,0 +1,68 @@
+/*
+ * SYSCTL.h
+ *
+ *  Created on: Jan 30, 2018
+ *      Author: vuong
+ */
+
+#ifndef SYSCTL_H_
+#define SYSCTL_H_
+
+#include <stdint.h>
+
+#define SYSCTL                      (((volatile uint32_t *)0x400FE000)) //System control base address
+
+enum {
+
+        SYSCTL_RCGCPWM          =   (0x640 >> 2),   //RCGC PWM1 offset
+#define SYSCTL_RCGC_M1PWMn          (1 << 1)       //Enable PWM 1 module
+        SYSCTL_RCGCGPIO         =   (0x608 >> 2),   //RCGC GPIO offset
+#define SYSCTL_RCGCGPIO_PORTB       (1 << 1)        //Enable run mode gating clock control
+#define SYSCTL_RCGCGPIO_PORTC       (1 << 2)        //for ports: B, C, D, and E
+#define SYSCTL_RCGCGPIO_PORTD       (1 << 3)
+#define SYSCTL_RCGCGPIO_PORTE       (1 << 4)
+        SYSCTL_RCGCI2C          =   (0x620 >> 2),   //RCGC I2C offset
+#define SYSCTL_RCGCI2C3             (1 << 3)        //Enable I2C3 module
+        SYSCTL_RCGCADC          =   (0x638 >> 2),   //RCGC ADC offset
+#define SYSCTL_RCGCADC0             (1 << 0)        //Enable ADC0 module
+#define SYSCTL_RCGCADC1             (1 << 1)        //Enable ADC1 module
+        SYSCTL_RCGCUART         =   (0x618 >> 2),   //RCGC UART offset
+#define SYSCTL_RCGCUART1            (1 << 1)        //Enable UART1 module
+
+};
+
+enum {
+
+        SYSCTL_RCC_R            =   (0x060 >> 2),   //Run-mode clock configuration register
+#define SYSCTL_RCC_USESYSDIV        (1 << 22)       //System clock is used undivided
+#define SYSCTL_RCC_XTAL             (0x15 << 6)     //16MHz crystal value
+#define SYSCTL_RCC_USEPWMDIV        (1 << 20)      //Enable PWM Clock. The PWM clock divider is the source for the PWM clock.
+#define SYSCTL_RCC_PWMDIV           (0x2 << 17)    //Value Divisor
+                                                    //0x0   /2
+                                                    //0x1   /4
+                                                    //0x2   /8
+                                                    //0x3   /16
+                                                    //0x4   /32
+                                                    //0x5   /64
+                                                    //0x6   /64
+                                                    //0x7   /64 (default)
+        SYSCTL_RCC2_R           =   (0x070 >> 2),   //Run-mode clock 2 configuration register
+#define SYSCTL_RCC2_BYPASS2         (1 << 11)       //Disable the PLL Bypass2, the system clock is the PLL output clock
+                                                    //divided by the divisor specified by SYSDIV2.
+                                                    //See Table 5-5 on page 223 for programming guidelines.
+                                                    //Note: The ADC must be clocked from the PLL or directly from a
+                                                    //16-MHz clock source to operate properly.
+#define SYSCTL_RCC2_USERCC2         ((uint32_t)1<<31)//The RCC2 register fields override the RCC register fields.
+#define SYSCTL_RCC2_OSCSRC2         (0x1 << 4)      //Select main oscillator.
+#define SYSCTL_RCC2_PWRDN2          (1 << 13)       //Clear this bit to power and enable PLL and output
+#define SYSCTL_RCC2_SYSDIV2         (0x02 << 23)    //System Clock Divisor 2
+#define SYSCTL_RCC2_SYSDIV2LSB      (1 << 22)       //Additional LSB for SYSDIV2
+#define SYSCTL_RCC2_SYSDIV400       (1 << 30)       //Enabling, Append the SYSDIV2LSB bit to the SYSDIV2 field to create a
+                                                    //7 bit divisor using the 400 MHz PLL output, see Table
+                                                    //5-6 on page 224.
+        SYSCTL_RIS_R            =   (0x050 >> 2)    //Raw interrupt status register
+#define SYSCTL_RIS_PLLLRIS          (1 << 6)        //PLL Lock Raw Interrupt Status
+
+};
+
+#endif /* SYSCTL_H_ */
